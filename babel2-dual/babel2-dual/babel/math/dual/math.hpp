9fd5d0e9c288cd66ec2f084a1c11b305d2dfa93e
SS <zakktanaka@yahoo.co.jp>
2019-03-14 22:52:40 +0900

    namespace math

--- babel2-dual/babel2-dual/babel/math/dual/math.hpp
+++ babel2-dual/babel2-dual/babel/math/dual/math.hpp
@@ -7,19 +7,21 @@ namespace babel {
 	namespace math {
 		namespace dual {
 
+			namespace math = babel::math;
+
 			struct Exp : public UnaryFunction {
 				Exp(std::shared_ptr<Expression> hs) : 
 					UnaryFunction(
 					hs, 
-					babel::math::exp(hs->real()), 
-					babel::math::exp(hs->real())) {}
+					math::exp(hs->real()), 
+					math::exp(hs->real())) {}
 			};
 
 			struct Log : public UnaryFunction {
 				Log(std::shared_ptr<Expression> hs) : 
 					UnaryFunction(
 						hs, 
-						babel::math::log(hs->real()),
+						math::log(hs->real()),
 						1 / hs->real()) {}
 			};
 
@@ -27,16 +29,16 @@ namespace babel {
 				Sqrt(std::shared_ptr<Expression> hs) : 
 					UnaryFunction(
 						hs, 
-						babel::math::sqrt(hs->real()),
-						0.5 / babel::math::sqrt(hs->real())) {}
+						math::sqrt(hs->real()),
+						0.5 / math::sqrt(hs->real())) {}
 			};
 
 			struct Pow : public UnaryFunction {
 				Pow(std::shared_ptr<Expression> hs, const RealType& p) : 
 					UnaryFunction(
 						hs, 
-						babel::math::pow(hs->real(), p),
-						p * babel::math::pow(hs->real(), p - 1)) {}
+						math::pow(hs->real(), p),
+						p * math::pow(hs->real(), p - 1)) {}
 			};
 
 		}
