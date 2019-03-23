19fb1947df3fe00d455d5eac30239196a153d431
SS <zakktanaka@hotmail.com>
2019-03-23 22:24:36 +0900

    update

--- babel2-dual/babel2-dual-test/hiho/TestAmericanOption.cpp
+++ babel2-dual/babel2-dual-test/hiho/TestAmericanOption.cpp
@@ -4,5 +4,5 @@
 TEST(AO, Test) {
 	auto p = hiho::putAmericanOption(100, 0.2, 100, 0.005, 3, 1000);
 
-	EXPECT_DOUBLE_EQ(13.122086669993108, p.real());
+	EXPECT_DOUBLE_EQ(13.494113143178293, p.real());
 }
