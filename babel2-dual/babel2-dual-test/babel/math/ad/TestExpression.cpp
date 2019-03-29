1e140f75dc41ac6cb650ce818d4b75edbec3c5fe
SS <zakktanaka@hotmail.com>
2019-03-29 20:50:32 +0900

    update

--- babel2-dual/babel2-dual-test/babel/math/ad/TestExpression.cpp
+++ babel2-dual/babel2-dual-test/babel/math/ad/TestExpression.cpp
@@ -2,102 +2,92 @@
 #include <babel/math/ad/Expression.hpp>
 
 using namespace babel::math::ad::expression;
-using Generator  = ExpressionGenerator<double>;
+using E  = babel::math::ad::expression::Expression<double>;
 
 TEST(TestExpression, Test) {
-	Generator gen;
+	auto a0 = E{ 10 };
+	auto a1 = E{ 20, { { 2, &a0 } } };
+	auto a2 = E{ 30, { { 3, &a0 }, { 4, &a1 } } };
 
-	auto a0 = gen(10);
-	auto a1 = gen(20, { { 2, a0 } } );
-	auto a2 = gen(30, { { 3, a0 }, { 4, a1 } } );
-
-	EXPECT_EQ(11, a2->differential(a0));
+	EXPECT_EQ(11, a2.differential(a0));
 }
 
 TEST(TestExpression, Add) {
-	Generator gen;
+	E x{ 10 };
+	E y{ 20 };
 
-	auto x = gen(10);
-	auto y = gen(20);
+	auto a0 = add(x, 5);
+	EXPECT_EQ(10 + 5, a0.x);
+	EXPECT_EQ(1, a0.differential(x));
+	EXPECT_EQ(0, a0.differential(y));
 
-	auto a0 = add(x, 5.0, gen);
-	EXPECT_EQ(10 + 5, a0->x);
-	EXPECT_EQ(1, a0->differential(x));
-	EXPECT_EQ(0, a0->differential(y));
+	auto a1 = add(3.0, y);
+	EXPECT_EQ(3 + 20, a1.x);
+	EXPECT_EQ(0, a1.differential(x));
+	EXPECT_EQ(1, a1.differential(y));
 
-	auto a1 = add(3.0, y, gen);
-	EXPECT_EQ(3 + 20, a1->x);
-	EXPECT_EQ(0, a1->differential(x));
-	EXPECT_EQ(1, a1->differential(y));
-
-	auto a2 = add(x, y, gen);
-	EXPECT_EQ(10 + 20, a2->x);
-	EXPECT_EQ(1, a2->differential(x));
-	EXPECT_EQ(1, a2->differential(y));
+	auto a2 = add(x, y);
+	EXPECT_EQ(10 + 20, a2.x);
+	EXPECT_EQ(1, a2.differential(x));
+	EXPECT_EQ(1, a2.differential(y));
 }
 
 TEST(TestExpression, Subtract) {
-	Generator gen;
+	E x{ 10 };
+	E y{ 20 };
 
-	auto x = gen(10);
-	auto y = gen(20);
+	auto a0 = subtract(x, 5);
+	EXPECT_EQ(10 - 5, a0.x);
+	EXPECT_EQ(1, a0.differential(x));
+	EXPECT_EQ(0, a0.differential(y));
 
-	auto a0 = subtract(x, 5.0, gen);
-	EXPECT_EQ(10 - 5, a0->x);
-	EXPECT_EQ(1, a0->differential(x));
-	EXPECT_EQ(0, a0->differential(y));
+	auto a1 = subtract(3, y);
+	EXPECT_EQ(3 - 20, a1.x);
+	EXPECT_EQ(0, a1.differential(x));
+	EXPECT_EQ(-1, a1.differential(y));
 
-	auto a1 = subtract(3.0, y, gen);
-	EXPECT_EQ(3 - 20, a1->x);
-	EXPECT_EQ(0, a1->differential(x));
-	EXPECT_EQ(-1, a1->differential(y));
-
-	auto a2 = subtract(x, y, gen);
-	EXPECT_EQ(10 - 20, a2->x);
-	EXPECT_EQ(1, a2->differential(x));
-	EXPECT_EQ(-1, a2->differential(y));
+	auto a2 = subtract(x, y);
+	EXPECT_EQ(10 - 20, a2.x);
+	EXPECT_EQ(1, a2.differential(x));
+	EXPECT_EQ(-1, a2.differential(y));
 }
 
 TEST(TestExpression, Times) {
-	Generator gen;
+	E x{ 10 };
+	E y{ 20 };
 
-	auto x = gen(10);
-	auto y = gen(20);
+	auto a0 = times(x, 5);
+	EXPECT_EQ(10 * 5, a0.x);
+	EXPECT_EQ(5, a0.differential(x));
+	EXPECT_EQ(0, a0.differential(y));
 
-	auto a0 = times(x, 5.0, gen);
-	EXPECT_EQ(10 * 5, a0->x);
-	EXPECT_EQ(5, a0->differential(x));
-	EXPECT_EQ(0, a0->differential(y));
+	auto a1 = times(3, y);
+	EXPECT_EQ(3 * 20, a1.x);
+	EXPECT_EQ(0, a1.differential(x));
+	EXPECT_EQ(3, a1.differential(y));
 
-	auto a1 = times(3.0, y, gen);
-	EXPECT_EQ(3 * 20, a1->x);
-	EXPECT_EQ(0, a1->differential(x));
-	EXPECT_EQ(3, a1->differential(y));
-
-	auto a2 = times(x, y, gen);
-	EXPECT_EQ(10 * 20, a2->x);
-	EXPECT_EQ(20, a2->differential(x));
-	EXPECT_EQ(10, a2->differential(y));
+	auto a2 = times(x, y);
+	EXPECT_EQ(10 * 20, a2.x);
+	EXPECT_EQ(20, a2.differential(x));
+	EXPECT_EQ(10, a2.differential(y));
 }
 
 TEST(TestExpression, Divide) {
-	Generator gen;
+	E x{ 10 };
+	E y{ 20 };
 
-	auto x = gen(10);
-	auto y = gen(20);
+	auto a0 = divide(x, 5);
+	EXPECT_EQ(10.0 / 5, a0.x);
+	EXPECT_EQ(1.0 / 5, a0.differential(x));
+	EXPECT_EQ(0, a0.differential(y));
 
-	auto a0 = divide(x, 5.0, gen);
-	EXPECT_EQ(10.0 / 5, a0->x);
-	EXPECT_EQ(1.0 / 5, a0->differential(x));
-	EXPECT_EQ(0, a0->differential(y));
+	auto a1 = divide(3, y);
+	EXPECT_EQ(3.0 / 20, a1.x);
+	EXPECT_EQ(0, a1.differential(x));
+	EXPECT_EQ(-1.0/400, a1.differential(y));
 
-	auto a1 = divide(3.0, y, gen);
-	EXPECT_EQ(3.0 / 20, a1->x);
-	EXPECT_EQ(0, a1->differential(x));
-	EXPECT_EQ(-1.0/400, a1->differential(y));
-
-	auto a2 = divide(x, y, gen);
-	EXPECT_EQ(10.0 / 20, a2->x);
-	EXPECT_EQ(1.0 / 20, a2->differential(x));
-	EXPECT_EQ(-10.0 / 400, a2->differential(y));
+	auto a2 = divide(x, y);
+	EXPECT_EQ(10.0 / 20, a2.x);
+	EXPECT_EQ(1.0 / 20, a2.differential(x));
+	EXPECT_EQ(-10.0 / 400, a2.differential(y));
 }
--- babel2-dual/babel2-dual/babel/math/ad/Expression.hpp
+++ babel2-dual/babel2-dual/babel/math/ad/Expression.hpp
@@ -18,8 +18,9 @@ namespace babel {
 					const ValueType  x;
 					const Polynomial polynomial;
 
-					ValueType differential(const ThisType* h) const {
-						if (h == this) {
+					ValueType differential(const ThisType& h) const {
+						auto p = &h;
+						if (p == this) {
 							return 1;
 						}
 
@@ -31,110 +32,68 @@ namespace babel {
 					}
 				};
 
-				template<typename ValueType_>
-				struct  ExpressionGenerator {
-					using ValueType  = ValueType_;
-					using Expression = Expression<ValueType>;
-					using Polynomial = typename Expression::Polynomial;
-
-					std::list<Expression> expressions;
-
-					const Expression* operator()(ValueType x, Polynomial polynomial) {
-						expressions.emplace_back(Expression{ x, polynomial });
-						return &*std::rbegin(expressions);
-					}
-
-					const Expression* operator()(ValueType x) {
-						return (*this)(x, {});
-					}
-				};
-
-				template<typename ValueType_>
-				inline const Expression<ValueType_>* add(
-					const Expression<ValueType_>* lhs, 
-					const Expression<ValueType_>* rhs, 
-					ExpressionGenerator<ValueType_>& gen) {
-					return gen( lhs->x + rhs->x, { {ValueType_(1), lhs}, {ValueType_(1), rhs} });
+				template<typename E>
+				inline E add(const E& lhs, const E& rhs) {
+					using ValueType = typename E::ValueType;
+					return E{ lhs.x + rhs.x, { {ValueType(1), &lhs}, {ValueType(1), &rhs} } };
 				}
-				template<typename ValueType_>
-				inline const Expression<ValueType_>* add(
-					ValueType_                    lhs, 
-					const Expression<ValueType_>* rhs, 
-					ExpressionGenerator<ValueType_>& gen) {
-					return gen(lhs + rhs->x, { {ValueType_(1), rhs} });
+				template<typename E>
+				inline E add(typename E::ValueType lhs, const E& rhs) {
+					using ValueType = typename E::ValueType;
+					return E{ lhs + rhs.x, { {ValueType(1), &rhs} } };
 				}
-				template<typename ValueType_>
-				inline const Expression<ValueType_>* add(
-					const Expression<ValueType_>* lhs,
-					ValueType_                    rhs,
-					ExpressionGenerator<ValueType_>& gen) {
-					return gen((lhs->x + rhs), { {ValueType_(1), lhs} });
+				template<typename E>
+				inline E add(const E& lhs, typename E::ValueType rhs) {
+					using ValueType = typename E::ValueType;
+					return E{ lhs.x + rhs, { {ValueType(1), &lhs} } };
 				}
 
-				template<typename ValueType_>
-				inline const Expression<ValueType_>* subtract(
-					const Expression<ValueType_>* lhs,
-					const Expression<ValueType_>* rhs,
-					ExpressionGenerator<ValueType_>& gen) {
-					return gen(lhs->x - rhs->x, { {ValueType_(1), lhs}, {ValueType_(-1), rhs} });
+				template<typename E>
+				inline E subtract(const E& lhs, const E& rhs) {
+					using ValueType = typename E::ValueType;
+					return E{ lhs.x - rhs.x, { {ValueType(1), &lhs}, {ValueType(-1), &rhs} } };
 				}
-				template<typename ValueType_>
-				inline const Expression<ValueType_>* subtract(
-					ValueType_                    lhs,
-					const Expression<ValueType_>* rhs,
-					ExpressionGenerator<ValueType_>& gen) {
-					return gen(lhs - rhs->x, { {ValueType_(-1), rhs} });
+				template<typename E>
+				inline E subtract(typename E::ValueType lhs, const E& rhs) {
+					using ValueType = typename E::ValueType;
+					return E{ lhs - rhs.x, { {ValueType(-1), &rhs} } };
 				}
-				template<typename ValueType_>
-				inline const Expression<ValueType_>* subtract(
-					const Expression<ValueType_>* lhs,
-					ValueType_                    rhs,
-					ExpressionGenerator<ValueType_>& gen) {
-					return gen(lhs->x - rhs, { {ValueType_(1), lhs} });
+				template<typename E>
+				inline E subtract(const E& lhs, typename E::ValueType rhs) {
+					using ValueType = typename E::ValueType;
+					return E{ lhs.x - rhs, { {ValueType(1), &lhs} } };
 				}
 
-				template<typename ValueType_>
-				inline const Expression<ValueType_>* times(
-					const Expression<ValueType_>* lhs,
-					const Expression<ValueType_>* rhs,
-					ExpressionGenerator<ValueType_>& gen) {
-					return gen(lhs->x * rhs->x, { {rhs->x, lhs}, {lhs->x, rhs} });
+				template<typename E>
+				inline E times(const E& lhs, const E& rhs) {
+					using ValueType = typename E::ValueType;
+					return E{ lhs.x * rhs.x, { {rhs.x, &lhs}, {lhs.x, &rhs} } };
 				}
-				template<typename ValueType_>
-				inline const Expression<ValueType_>* times(
-					ValueType_                    lhs,
-					const Expression<ValueType_>* rhs,
-					ExpressionGenerator<ValueType_>& gen) {
-					return gen(lhs * rhs->x, { {lhs, rhs} });
+				template<typename E>
+				inline E times(typename E::ValueType lhs, const E& rhs) {
+					using ValueType = typename E::ValueType;
+					return E{ lhs * rhs.x, { {lhs, &rhs} } };
 				}
-				template<typename ValueType_>
-				inline const Expression<ValueType_>* times(
-					const Expression<ValueType_>* lhs,
-					ValueType_                    rhs,
-					ExpressionGenerator<ValueType_>& gen) {
-					return gen(lhs->x * rhs, { {rhs, lhs} });
+				template<typename E>
+				inline E times(const E& lhs, typename E::ValueType rhs) {
+					using ValueType = typename E::ValueType;
+					return E{ lhs.x * rhs, { {rhs, &lhs} } };
 				}
 
-				template<typename ValueType_>
-				inline const Expression<ValueType_>* divide(
-					const Expression<ValueType_>* lhs,
-					const Expression<ValueType_>* rhs,
-					ExpressionGenerator<ValueType_>& gen) {
-					return gen(lhs->x / rhs->x, { {ValueType_(1) / rhs->x, lhs}, {ValueType_(-1) * lhs->x / (rhs->x * rhs->x), rhs} });
+				template<typename E>
+				inline E divide(const E& lhs, const E& rhs) {
+					using ValueType = typename E::ValueType;
+					return E{ lhs.x / rhs.x, { {ValueType(1) / rhs.x, &lhs}, {ValueType(-1) * lhs.x / (rhs.x * rhs.x), &rhs} } };
 				}
-				template<typename ValueType_>
-				inline const Expression<ValueType_>* divide(
-					ValueType_                    lhs,
-					const Expression<ValueType_>* rhs,
-					ExpressionGenerator<ValueType_>& gen) {
-					return gen(lhs / rhs->x, { {ValueType_(-1) / (rhs->x * rhs->x), rhs} });
+				template<typename E>
+				inline E divide(typename E::ValueType lhs, const E& rhs) {
+					using ValueType = typename E::ValueType;
+					return E{ lhs / rhs.x, { {ValueType(-1) / (rhs.x * rhs.x), &rhs} } };
 				}
-				template<typename ValueType_>
-				inline const Expression<ValueType_>* divide(
-					const Expression<ValueType_>* lhs,
-					ValueType_                    rhs,
-					ExpressionGenerator<ValueType_>& gen) {
-					return gen(lhs->x / rhs, { { ValueType_(1) / rhs, lhs} });
+				template<typename E>
+				inline E divide(const E& lhs, typename E::ValueType rhs) {
+					using ValueType = typename E::ValueType;
+					return E{ lhs.x / rhs, { { ValueType(1) / rhs, &lhs} } };
 				}
 
 			}
