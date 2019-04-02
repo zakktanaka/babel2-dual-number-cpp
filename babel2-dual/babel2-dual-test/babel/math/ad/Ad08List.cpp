af624a7211218722b83dc79d5ce1e2d752b27730
SS <zakktanaka@hotmail.com>
2019-04-02 23:09:04 +0900

    ad08

---
+++ babel2-dual/babel2-dual-test/babel/math/ad/Ad08List.cpp
@@ -0,0 +1,174 @@
+#include <pch.h>
+#include <cmath>
+#include <chrono>
+#include <utility>
+#include <array>
+#include <vector>
+#include <list>
+#include <functional>
+#include <unordered_map>
+
+namespace {
+
+	namespace math {
+
+		using ValueType = double;
+		using Cache = std::unordered_map<const void*, ValueType>;
+
+		struct Expression {
+			using Term = std::pair<ValueType, Expression*>;
+			using Polynomial = std::array<Term, 2>;
+
+			static std::list<Expression> expressions;
+
+			static Expression* newExpression() {
+				return nullptr;
+			}
+			static Expression* newExpression(const Polynomial& pol) {
+				expressions.emplace_back(Expression{ pol });
+				return &*std::rbegin(expressions);
+			}
+			static Expression* newExpression(Polynomial&& pol) {
+				expressions.emplace_back(Expression{ pol });
+				return &*std::rbegin(expressions);
+			}
+
+			Polynomial polynomial;
+
+			ValueType d(Expression* other, Cache& cache) const {
+				if (this == other) { return 1; }
+
+				const void*  x = &*this;
+				auto it = cache.find(x);
+				if (it != std::end(cache)) {
+					return it->second;
+				}
+
+				ValueType dx = 0;
+				for (auto& term : polynomial) {
+					if (term.second != 0) {
+						auto a = term.first;
+						auto e = term.second;
+						dx += a * e->d(other, cache);
+					}
+				}
+				cache.emplace(x, dx);
+				return dx;
+			}
+		};
+
+		std::list<Expression> Expression::expressions = { };
+
+		struct Number {
+			using Polynomial = Expression::Polynomial;
+
+			Expression* eindex;
+			ValueType v;
+			Number() : eindex(Expression::newExpression()), v{ 0 } {}
+			Number(ValueType vv) :eindex(Expression::newExpression(Polynomial{ { {0,0}, {0,0} } })), v{ vv } {}
+			Number(ValueType vv, ValueType a0, const Number& x0) :
+				eindex(Expression::newExpression(Polynomial{ { {a0,x0.eindex}, {0,0} } })),
+				v{ vv } {}
+			Number(ValueType vv, ValueType a0, const Number& x0, ValueType a1, const Number& x1) :
+				eindex(Expression::newExpression(Polynomial{ { {a0,x0.eindex}, {a1,x1.eindex} } })),
+				v{ vv } {}
+			ValueType d(const Number& dx) const { Cache cache; return eindex->d(dx.eindex, cache); }
+			Number operator-() const { return Number{ -v, -1, *this }; }
+		};
+		Number operator+(const Number& l, const Number& r) { return Number{ l.v + r.v, 1, l, 1, r, }; }
+		Number operator-(const Number& l, const Number& r) { return Number{ l.v - r.v, 1, l, -1, r, }; }
+		Number operator*(const Number& l, const Number& r) { return Number{ l.v * r.v, r.v, l, l.v, r, }; }
+		Number operator/(const Number& l, const Number& r) {
+			auto ll = l.v;
+			auto rr = r.v;
+			return Number{ l.v / r.v, 1.0 / rr, l, -ll / (rr * rr), r, };
+		}
+		Number operator+(const Number& l, ValueType r) { return Number{ l.v + r, 1, l, }; }
+		Number operator-(const Number& l, ValueType r) { return Number{ l.v - r, 1, l, }; }
+		Number operator*(const Number& l, ValueType r) { return Number{ l.v * r, r, l, }; }
+		Number operator/(const Number& l, ValueType r) { return Number{ l.v / r, 1.0 / r, l, }; }
+		Number operator+(ValueType l, const Number& r) { return Number{ l + r.v, 1, r, }; }
+		Number operator-(ValueType l, const Number& r) { return Number{ l - r.v, -1, r, }; }
+		Number operator*(ValueType l, const Number& r) { return Number{ l * r.v, l, r, }; }
+		Number operator/(ValueType l, const Number& r) { return Number{ l / r.v, -l / (r.v * r.v), r, }; }
+		bool operator>(const Number& l, const Number& r) { return l.v > r.v; }
+		Number exp(const Number& l) {
+			auto ll = std::exp(l.v);
+			return Number{ ll, ll, l, };
+		}
+		Number sqrt(const Number& l) {
+			auto ll = std::sqrt(l.v);
+			return Number{ ll, ll, l, };
+		}
+		Number pow(const Number& l, ValueType r) {
+			auto ll = std::pow(l.v, r);
+			return Number{ ll, r * ll / l.v, l, };
+		}
+
+		using std::exp;
+		using std::sqrt;
+		using std::pow;
+	}
+
+	using Real = math::Number;
+
+	inline Real putAmericanOption(const Real& s, const Real& sigma, const Real& k, const Real& r, const Real& t, int simulation) {
+
+		auto dt = t / simulation;
+		auto up = math::exp(sigma * math::sqrt(dt));
+
+		auto p0 = (up - math::exp(-r * dt)) / (up * up - 1);
+		auto p1 = math::exp(-r * dt) - p0;
+
+		std::vector<Real> p;
+		for (int i = 0; i != simulation; ++i) {
+			auto pp = k - s * math::pow(up, 2.0 * i - simulation);
+			pp = pp > 0.0 ? pp : 0.0;
+			p.push_back(pp);
+		}
+
+		for (int j = simulation - 1; j != 0; --j) {
+			for (int i = 0; i != j; ++i) {
+				p[i] = p0 * p[i + 1] + p1 * p[i];    // binomial value
+				auto exercise = k - s * math::pow(up, 2.0 * i - j);  // exercise value
+				p[i] = p[i] > exercise ? p[i] : exercise;
+			}
+		}
+
+		return p[0];
+	}
+
+	struct Timer {
+		using Clock = std::chrono::system_clock;
+		using TimePoint = std::chrono::time_point<Clock>;
+
+		TimePoint start;
+		TimePoint end;
+		Real      ans;
+		Timer(std::function<Real()> f) {
+			start = Clock::now();
+			ans = f();
+			end = Clock::now();
+		}
+
+		long long duration() {
+			return std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+		}
+	};
+}
+
+TEST(Ad08, Test) {
+
+	Real s(100);
+	auto f = [&s]() {return putAmericanOption(s, 0.2, 100, 0.005, 3, 1000); };
+	Timer t(f);
+
+	EXPECT_DOUBLE_EQ(13.494113143178293, t.ans.v);
+
+	// real = double 17 milli sec
+	// real = simple struct 30 milli sec
+	EXPECT_LE(440, t.duration());
+	EXPECT_GT(480, t.duration());
+
+	EXPECT_DOUBLE_EQ(-0.42779717603899964, t.ans.d(s));
+}
