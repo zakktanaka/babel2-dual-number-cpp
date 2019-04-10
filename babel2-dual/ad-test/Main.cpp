c3e16c9e91e3ed17b9b3504d5434072480600133
SS <zakktanaka@yahoo.co.jp>
2019-04-10 22:26:14 +0900

    pmr

--- babel2-dual/ad-test/Main.cpp
+++ babel2-dual/ad-test/Main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <memory_resource>
 
 int main() {
+
+	std::pmr::synchronized_pool_resource mem_res;
+
+	std::pmr::list<int> v(&mem_res);
+
+	for (int i = 0; i < 100000; ++i) {
+		v.emplace_back(i);
+	}
+
+	auto it = std::begin(v);
+	while (it != std::end(v)) {
+		it = v.erase(it);
+	}
 
 	std::cin.ignore();
 
