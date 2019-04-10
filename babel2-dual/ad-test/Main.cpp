49dff2a6dbe1dbefcfb8befacf8105d966d92c78
SS <zakktanaka@yahoo.co.jp>
2019-04-10 23:26:36 +0900

    ad00

--- babel2-dual/ad-test/Main.cpp
+++ babel2-dual/ad-test/Main.cpp
@@ -1,26 +1,10 @@
 #include <iostream>
 #include <memory_resource>
-#include "hiho/Timer.hpp"
+#include "hiho/Ad.hpp"
 
 int main() {
 
-	auto timer = hiho::newTimer([]() {return 100; });
-
-	std::cout << timer.value << std::endl;
-	std::cout << timer.duration() << std::endl;
-
-	std::pmr::synchronized_pool_resource mem_res;
-
-	std::pmr::list<int> v(&mem_res);
-
-	for (int i = 0; i < 100000; ++i) {
-		v.emplace_back(i);
-	}
-
-	auto it = std::begin(v);
-	while (it != std::end(v)) {
-		it = v.erase(it);
-	}
+	hiho::ad00(100, 0.2, 100, 0.005, 3, 1000);
 
 	std::cin.ignore();
 
---
+++ babel2-dual/ad-test/hiho/Ad.hpp
@@ -0,0 +1,6 @@
+#pragma once
+namespace hiho {
+
+	void ad00(double s, double sigma, double k, double r, double t, int simulation);
+
+}
