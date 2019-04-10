92c42fa49450bc8450086df70bc1f951aef2404f
SS <zakktanaka@yahoo.co.jp>
2019-04-10 23:42:50 +0900

    update

--- babel2-dual/ad-test/hiho/Ad.hpp
+++ babel2-dual/ad-test/hiho/Ad.hpp
@@ -1,6 +1,8 @@
 #pragma once
 namespace hiho {
 
+	double american(double s, double sigma, double k, double r, double t, int simulation);
+
 	void ad00(double s, double sigma, double k, double r, double t, int simulation);
 
 }
--- babel2-dual/ad-test/hiho/Ad00.cpp
+++ babel2-dual/ad-test/hiho/Ad00.cpp
@@ -1,53 +1,13 @@
 #include "Ad.hpp"
 
-#include <cmath>
 #include <iostream>
-#include "Ad.hpp"
 #include "Timer.hpp"
-
-namespace {
-
-	namespace math {
-		using std::exp;
-		using std::sqrt;
-		using std::pow;
-	}
-
-	using Real = double;
-
-	inline Real americanPutOption(const Real& s, const Real& sigma, const Real& k, const Real& r, const Real& t, int simulation) {
-
-		auto dt = t / simulation;
-		auto up = math::exp(sigma * math::sqrt(dt));
-
-		auto p0 = (up - math::exp(-r * dt)) / (up * up - 1);
-		auto p1 = math::exp(-r * dt) - p0;
-
-		std::vector<Real> p;
-		for (int i = 0; i != simulation; ++i) {
-			auto pp = k - s * math::pow(up, 2.0 * i - simulation);
-			pp = pp > 0.0 ? pp : 0.0;
-			p.push_back(pp);
-		}
-
-		for (int j = simulation - 1; j != 0; --j) {
-			for (int i = 0; i != j; ++i) {
-				p[i] = p0 * p[i + 1] + p1 * p[i];    // binomial value
-				auto exercise = k - s * math::pow(up, 2.0 * i - j);  // exercise value
-				p[i] = p[i] > exercise ? p[i] : exercise;
-			}
-		}
-
-		return p[0];
-	}
-
-}
 
 
 void hiho::ad00(double s, double sigma, double k, double r, double t, int simulation)
 {
 	auto timer = hiho::newTimer(
-		[&]() { return americanPutOption(s, sigma, k, r, t, simulation); }
+		[&]() { return hiho::american(s, sigma, k, r, t, simulation); }
 	);
 
 	std::cout << "ad00 double val : " << timer.value << ", time : " << timer.duration() << " msec" << std::endl;
