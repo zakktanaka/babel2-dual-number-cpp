75fe2090406ead70b80a31fc02e1df2f3b711ef7
SS <zakktanaka@yahoo.co.jp>
2019-04-11 18:57:04 +0900

    ad01

--- babel2-dual/ad-test/hiho/Ad.hpp
+++ babel2-dual/ad-test/hiho/Ad.hpp
@@ -4,5 +4,6 @@ namespace hiho {
 	double american(double s, double sigma, double k, double r, double t, int simulation);
 
 	void ad00(double s, double sigma, double k, double r, double t, int simulation);
+	void ad01(double s, double sigma, double k, double r, double t, int simulation);
 
 }
