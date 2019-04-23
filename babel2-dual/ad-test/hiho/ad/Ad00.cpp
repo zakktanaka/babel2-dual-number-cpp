99453d98533cf6d36bd92f2462eb1721429f783d
SS <zakktanaka@hotmail.com>
2019-04-23 23:15:38 +0900

    greeks

--- babel2-dual/ad-test/hiho/ad/Ad00.cpp
+++ babel2-dual/ad-test/hiho/ad/Ad00.cpp
@@ -11,5 +11,8 @@ void hiho::ad00(double s, double sigma, double k, double r, double t, int simula
 		[&]() { return hiho::american(s, sigma, k, r, t, simulation); }
 	);
 
-	std::cout << "ad00 double val : " << timer.value << ", time : " << timer.duration() << " msec" << std::endl;
+	std::cout << "ad00 double val : " << timer.value << ", time : " << timer.duration() << " msec" 		<< ", delta : " << (hiho::american(s + 0.0001, sigma, k, r, t, simulation) - hiho::american(s - 0.0001, sigma, k, r, t, simulation)) / 0.0001 /2
+		<< ", vega : " << (hiho::american(s, sigma + 0.0001, k, r, t, simulation) - hiho::american(s, sigma - 0.0001, k, r, t, simulation)) / 0.0001 / 2
+		<< ", theta : " << (hiho::american(s, sigma, k, r, t + 0.0001, simulation) - hiho::american(s, sigma, k, r, t - 0.0001, simulation)) / 0.0001 / 2
+		<< std::endl;
 }
